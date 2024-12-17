8db45b12c78b4282891bc16bf654b30589c9753b
Zul <zulkifl.alidou@gmail.com>
2024-12-16 17:17:41 -0800

    refactor: Change request object's datatype

--- src/main/java/com/BOMBLAND_server/DynamoDBEvent_Controller.java
+++ src/main/java/com/BOMBLAND_server/DynamoDBEvent_Controller.java
@@ -8,21 +8,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DynamoDBEvent_Controller {
     @PostMapping("/database-changes")
-    public ResponseEntity<String> handleInserts(@RequestBody String event) {
-//        System.out.println("Received DynamoDB Event: " + event.getMessage());
-        System.out.println("Received DynamoDB Event: " + event);
+    public ResponseEntity<String> handleInserts(@RequestBody HighScore newHighScore) {
+        System.out.println("\nnewHighScore: " + newHighScore);
+        System.out.println("newHighScore.id = " + newHighScore.getID());
+        System.out.println("newHighScore.score = " + newHighScore.getScore());
+        System.out.println("newHighScore.name = " + newHighScore.getName());
+
         return ResponseEntity.ok("Request processed successfully!");
     }
 }
 
-class DynamoDB_Event {
-    private String message;
+class HighScore {
+    private String id;
+    private int score;
+    private String name;
 
-    public String getMessage() {
-        return message;
+    public String getID() {
+        return id;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    public int getScore() {
+        return score;
+    }
+
+    public String getName() {
+        return name;
     }
 }
