05fd619eed04bf56214c3af9bf9a755895e10b24
Zul <zulkifl.alidou@gmail.com>
2025-01-19 07:19:34 -0800

    feat: print out webflux server port

--- src/main/java/com/BOMBLAND_server/EnvironmentVariablesController.java
+++ src/main/java/com/BOMBLAND_server/EnvironmentVariablesController.java
@@ -1,15 +1,20 @@
 package com.BOMBLAND_server;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EnvironmentVariablesController {
+//    @Value("{local.server.port}")
+//    private int port;
+
     @GetMapping("/get-environment-variables")
     public EnvironmentVariablesReponse getEnvironmentVariables() {
         EnvironmentVariablesReponse envVarResp = new EnvironmentVariablesReponse(System.getenv("IDENTITY_POOL_ID"));
         System.out.println("getEnvironmentVariables()");
         System.out.println(envVarResp);
+        System.out.println("webflux server running on port = " + System.getProperty("local.server.port"));
 
         return envVarResp;
     }
