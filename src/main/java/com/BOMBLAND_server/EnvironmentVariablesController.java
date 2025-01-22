377ae1ee82451aeeb711a3b49af321cb27a77038
Zul <zulkifl.alidou@gmail.com>
2025-01-22 09:15:50 -0800

    test

--- src/main/java/com/BOMBLAND_server/EnvironmentVariablesController.java
+++ src/main/java/com/BOMBLAND_server/EnvironmentVariablesController.java
@@ -5,25 +5,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EnvironmentVariablesController {
-    @GetMapping("/get-environment-variables")
-    public EnvironmentVariablesReponse getEnvironmentVariables() {
-        EnvironmentVariablesReponse envVarResp = new EnvironmentVariablesReponse(System.getenv("IDENTITY_POOL_ID"));
-        System.out.println("getEnvironmentVariables()");
-        System.out.println(envVarResp);
-        System.out.println("webflux server running on port = " + System.getenv("PORT"));
-
-        return envVarResp;
-    }
+//    @GetMapping("/get-environment-variables")
+//    public EnvironmentVariablesReponse getEnvironmentVariables() {
+//        EnvironmentVariablesReponse envVarResp = new EnvironmentVariablesReponse(System.getenv("IDENTITY_POOL_ID"));
+//        System.out.println("getEnvironmentVariables()");
+//        System.out.println(envVarResp);
+//        System.out.println("webflux server running on port = " + System.getenv("PORT"));
+//
+//        return envVarResp;
+//    }
 }
 
-class EnvironmentVariablesReponse {
-    private String identityPoolID;
-
-    public EnvironmentVariablesReponse(String identityPoolID) {
-        this.identityPoolID = identityPoolID;
-    }
-
-    public String getIdentityPoolID() {
-        return identityPoolID;
-    }
-}
+//class EnvironmentVariablesReponse {
+//    private String identityPoolID;
+//
+//    public EnvironmentVariablesReponse(String identityPoolID) {
+//        this.identityPoolID = identityPoolID;
+//    }
+//
+//    public String getIdentityPoolID() {
+//        return identityPoolID;
+//    }
+//}
--- src/main/java/com/BOMBLAND_server/WebFluxRouterConfig.java
+++ src/main/java/com/BOMBLAND_server/WebFluxRouterConfig.java
@@ -10,12 +10,13 @@ import reactor.core.publisher.Mono;
 @Configuration
 public class WebFluxRouterConfig {
     @Bean
-    public RouterFunction<ServerResponse> route() {
-        return RouterFunctions
-                // Define routes
-                .route(RequestPredicates.GET("/webflux/"), this::handleDefaultRoute)
-                .andRoute(RequestPredicates.GET("/webflux/route2"), this::handleRoute2)
-                .andRoute(RequestPredicates.GET("/webflux/get-environment-variables"), this::handleGetEnvironmentVariables);
+    public RouterFunction<ServerResponse> routes() {
+        return RouterFunctions.route()
+                .nest(RequestPredicates.path("/webflux"), builder -> builder
+                    .GET("/", this::handleDefaultRoute)
+                    .GET("/route2", this::handleRoute2)
+                    .GET("/get-environment-variables", this::getEnvironmentVariables))
+                .build();
     }
 
 
@@ -27,7 +28,25 @@ public class WebFluxRouterConfig {
         return ServerResponse.ok().bodyValue("\n---handleRoute2---...\n");
     }
 
-    private Mono<ServerResponse> handleGetEnvironmentVariables(ServerRequest request) {
-        return ServerResponse.ok().bodyValue("\n---handleGetEnvironmentVariables---...\n");
+    private Mono<ServerResponse> getEnvironmentVariables(ServerRequest request) {
+        EnvironmentVariablesReponse envVarResp = new EnvironmentVariablesReponse(System.getenv("IDENTITY_POOL_ID"));
+        System.out.println("getEnvironmentVariables()");
+        System.out.println(envVarResp);
+        System.out.println("webflux server running on port = " + System.getenv("PORT"));
+
+        return ServerResponse.ok().bodyValue(envVarResp);
+    }
+}
+
+
+class EnvironmentVariablesReponse {
+    private String identityPoolID;
+
+    public EnvironmentVariablesReponse(String identityPoolID) {
+        this.identityPoolID = identityPoolID;
+    }
+
+    public String getIdentityPoolID() {
+        return identityPoolID;
     }
 }
