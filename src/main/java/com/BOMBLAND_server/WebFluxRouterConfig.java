d7e38d496972ea07e16f6c64f84aa7df60c4f81d
Zul <zulkifl.alidou@gmail.com>
2025-01-22 04:19:35 -0800

    test

--- src/main/java/com/BOMBLAND_server/WebFluxRouterConfig.java
+++ src/main/java/com/BOMBLAND_server/WebFluxRouterConfig.java
@@ -3,6 +3,7 @@ package com.BOMBLAND_server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.*;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import reactor.core.publisher.Mono;
 
 
@@ -11,10 +12,22 @@ public class WebFluxRouterConfig {
     @Bean
     public RouterFunction<ServerResponse> route() {
         return RouterFunctions
-                .route(RequestPredicates.GET("/get-environment-variables"), this::handleGetEnvironmentVariables);
+                // Define routes
+                .route(RequestPredicates.GET("/"), this::handleDefaultRoute)
+                .andRoute(RequestPredicates.GET("/route2"), this::handleRoute2)
+                .andRoute(RequestPredicates.GET("/get-environment-variables"), this::handleGetEnvironmentVariables);
+    }
+
+
+    private Mono<ServerResponse> handleDefaultRoute(ServerRequest request) {
+        return ServerResponse.ok().bodyValue("\n---handleDefaultRoute---...\n");
+    }
+
+    private Mono<ServerResponse> handleRoute2(ServerRequest request) {
+        return ServerResponse.ok().bodyValue("\n---handleRoute2---...\n");
     }
 
     private Mono<ServerResponse> handleGetEnvironmentVariables(ServerRequest request) {
-        return ServerResponse.ok().bodyValue("Environment variables...");
+        return ServerResponse.ok().bodyValue("\n---handleGetEnvironmentVariables---...\n");
     }
 }
