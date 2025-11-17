451002fb9c6d2006e8dc0a3708632f3c63e17e60
Zul-Kifl Alidou <zulkifl.alidou@gmail.com>
2025-11-17 14:52:05 -0800

    refactor: Rebuild the project so render.com has the latest version

--- src/main/java/com/BOMBLAND_server/BomblandServerApplication.java
+++ src/main/java/com/BOMBLAND_server/BomblandServerApplication.java
@@ -10,10 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BomblandServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BomblandServerApplication.class, args);
-
-		System.out.println("\n===BomblandServerApplication====================");
-		System.out.println("WebFlux server running on port = " + System.getProperty("local.server.port"));
-		System.out.println("WebFlux server running on port = " + System.getenv("PORT"));
-		System.out.println("===BomblandServerApplication====================\n");
+		System.out.println("\n===BomblandServerApplication===\n");
 	}
 }
